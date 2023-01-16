023ba32b0cab7717bccb8336a22bb2e2534d95f3
rkdtldn <105910264+rkdtldn@users.noreply.github.com>
2023-01-16 13:17:29 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(score)
 })
+let score = 0
+score = randint(0, 100)
+if (score > 90) {
+    basic.showNumber(1)
+} else if (score > 80) {
+    basic.showNumber(2)
+} else if (score > 70) {
+    basic.showNumber(3)
+} else if (score > 60) {
+    basic.showNumber(4)
+} else {
+    basic.showNumber(5)
+}
